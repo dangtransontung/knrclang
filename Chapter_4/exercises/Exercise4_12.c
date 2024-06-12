12bcd6a2c58c700f36cae2cd8eccc971d2db0453
Tung Dang <dangtransontung@gmail.com>
2024-06-11 23:43:07 -0400

    add content

--- Chapter_4/exercises/Exercise4_12.c
+++ Chapter_4/exercises/Exercise4_12.c
@@ -1,19 +1,38 @@
 #include <stdio.h>
-#define MAX_LENGTH 100;
+#define MAX_LENGTH 100
 
 void recursive_itoa(int n, char s[]);
+int recursive_itoa_helper(int n, char s[]);
 
-int main() {
+int main() 
+{
+	char s[MAX_LENGTH];
+	recursive_itoa(-123, s);
 
+	printf("%s\n", s);
 }
 
-void recursive_itoa(int n, char s[]) {
-	if (n < 0) {
-		s[0] = '-';
-		n = -n
-	}
+void recursive_itoa(int n, char s[]) 
+{
+	int pos = recursive_itoa_helper(n, s);
+	s[pos] = '\0';
+}
 
+int recursive_itoa_helper(int n, char s[])
+{
 	if (n / 10 == 0) {
-		return s;
+		if (n < 0) {
+			s[0] = '-';
+			s[1] = '0' + (-n);
+			return 2;
+		} else 
+			s[0] = '0' + n;
+			return 1;
 	}
+
+	int pos = recursive_itoa_helper(n/10, s);
+	s[pos++] = '0' + ((n < 0) ? (-n)%10 : n%10);
+
+	return pos;
 }
+
